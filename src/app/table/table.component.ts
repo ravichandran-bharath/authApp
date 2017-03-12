b8adc095685845e0ff5b4b24bd44880134a04753
Bharath Ravichandran <mrbharath26@gmail.com>
2017-03-12 12:11:11 +0530

    select menu in search bar

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
